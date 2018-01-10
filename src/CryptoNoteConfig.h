88f930db344ece3115a0095e0f9bfb91b66dbfa6
joinity <18332291+joinity@users.noreply.github.com>
2018-01-10 15:31:50 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  84.176.189.155:17236;
+  "84.176.189.155:17236",
   //"your_seed_ip2.com:8080",
 };
 
